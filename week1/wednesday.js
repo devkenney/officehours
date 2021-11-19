092add7f0369de4d117149aacad354902a1a1863
Devin Kenney <dkenney@perscholas.org>
2021-11-19 17:55:22 -0500

    Week One Day One

---
+++ week1/wednesday.js
@@ -0,0 +1,76 @@
+// Nums
+//Strings
+//booleans
+//underfind
+//NaN -nut a number
+// null ?
+​
+// var ,
+// let, const 
+// let name = "Liv";
+// name = "Liubov";
+// console.log(name);
+​
+//conditions
+​
+//write a prog to know if person cat by vodka
+// const person ="Devin";
+// const age = 21;
+// const wantToHaveADrink = true;
+// if(age >= 21){
+// console.log(person + " Can have a drink");
+// }
+​
+// if(age >= 21){
+//     if(wantToHaveADrink === true){
+// console.log(person + " Can have a drink");
+//     }
+// }
+​
+// ==  softe 
+// === strong 
+​
+// >=
+// >
+// <
+// <=
+​
+// != - means the oposit
+// let n1 = 'two';
+// let n2 = 'Two';
+// console.log(n1==n2);
+ 
+// % 
+// for(let i=0; i<10; i ++){
+//     // console.log(i);
+//     if(i%2===0){
+//         console.log("Hello");
+//     }else if(i%2 !==0){
+//     console.log('world');
+//     }
+// };
+​
+// for( let i =10; i>=0; i--){
+//     console.log(i);
+// }
+​
+// let result = '234';
+// let num = 234;
+// console.log('My result '+ num ) //str
+// console.log(result -1) //num
+// console.log(result * 1);// num
+​
+// console.log(Math.PI);
+// console.log(Math.floor(3/2));
+// console.log(Math.ceil(3 / 2));
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+//i = index;
